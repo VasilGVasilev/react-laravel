a93e14782b00930a877065b4163dce9615e8e4ed
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-02-08 13:11:49 +0200

    signup fe done

--- app/react/README.md
+++ app/react/README.md
@@ -6,3 +6,6 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+### Why we store token in state as well as localStorage?
+We need token stored in state to have our routing logic work by determining if token is truthy or falsy
--- app/react/src/pages/Signup.jsx
+++ app/react/src/pages/Signup.jsx
@@ -1,20 +1,44 @@
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider.hook";
 
 const Signup = () => {
+
+    const {setToken, setUser} = useStateContext();
+    
     const onSubmit = (ev) => {
         ev.preventDefault();
-        const formData = new FormData(ev.target)
-        console.log(formData);
+        const formData = new FormData(ev.target);
+        const payload = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+            password_confirmation: formData.get('passwordConfirmed')
+
+
+        }
+
+        axiosClient.post('/signup', payload)
+            .then(({data})=>{
+                setToken(data.token); //when token state is updated, we go back to the logic in GuestLayout if token -> navigate to '/' -> no redirect to /login; redirect to /users but within defaultLayout by the power of router.js 'navigate to'
+                setUser(data.user); 
+            })
+            .catch(err=>{
+                const response = err.response;
+                if (response && response.status === 422){ //422 validation error, thus, coming from the server
+                    console.log(response.data.errors);
+                }
+            })
     }
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Login into your account</h1>
-                    <input name="fullName" placeholder="Full Name"/>
+                    <input name="name" placeholder="Full Name"/>
                     <input type="email" name="email" placeholder="Email Address"/>
                     <input type="password" name="password" placeholder="Password"/>
-                    <input type="password" placeholder="Password Confirmation"/>
+                    <input type="password" name="passwordConfirmed" placeholder="Password Confirmation"/>
                     <button className="btn btn-block">Sign up</button>
                     <p className="message">
                         Already registered? <Link to={"/login"}>Sign in</Link>
